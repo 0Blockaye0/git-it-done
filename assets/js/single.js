67ef218e5224fe6f17d7e46b4558016ff89bca50
_blockaye_ <blake_austinjohnson@icloud.com>
2021-03-17 13:29:49 -0500

    more than 30 repos link

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,6 +1,22 @@
 
 var issueContainerEl = document.querySelector("#issues-container");
 
+var limitWarningEl = document.querySelector("#limit-warning");
+
+var displayWarning = function (repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
+
+///////////////////
 var getRepoIssues = function (repo) {
     console.log(repo);
 
@@ -12,6 +28,11 @@ var getRepoIssues = function (repo) {
             response.json().then(function (data) {
                 // pass response data to dom function
                 displayIssues(data);
+
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         }
         else {
